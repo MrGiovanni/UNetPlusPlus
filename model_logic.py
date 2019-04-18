4a184612a0b57dfc3eb4c9abfd4b1cfc7e9274f5
Zongwei Zhou <zongweiz@asu.edu>
2019-04-18 10:10:54 -0700

    Update model_logic.py

--- model_logic.py
+++ model_logic.py
@@ -44,7 +44,7 @@ def dice_coef(y_true, y_pred):
 def bce_dice_loss(y_true, y_pred):
     return 0.5 * keras.losses.binary_crossentropy(y_true, y_pred) - dice_coef(y_true, y_pred)
 
-
+# Evaluation metric: IoU
 def compute_iou(img1, img2):
 
     img1 = np.array(img1)
@@ -70,6 +70,44 @@ def compute_iou(img1, img2):
         #database.display_image_mask_pairs(im1, im2)
 
     return IoU
+
+# Evaluation metric: Dice
+def compute_dice(im1, im2, empty_score=1.0):
+    """
+    Computes the Dice coefficient, a measure of set similarity.
+    Parameters
+    ----------
+    im1 : array-like, bool
+        Any array of arbitrary size. If not boolean, will be converted.
+    im2 : array-like, bool
+        Any other array of identical size. If not boolean, will be converted.
+    Returns
+    -------
+    dice : float
+        Dice coefficient as a float on range [0,1].
+        Maximum similarity = 1
+        No similarity = 0
+        Both are empty (sum eq to zero) = empty_score
+
+    Notes
+    -----
+    The order of inputs for `dice` is irrelevant. The result will be
+    identical if `im1` and `im2` are switched.
+    """
+    im1 = np.asarray(im1).astype(np.bool)
+    im2 = np.asarray(im2).astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    im_sum = im1.sum() + im2.sum()
+    if im_sum == 0:
+        return empty_score
+
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+
+    return 2. * intersection.sum() / im_sum
 
 
 ########################################
