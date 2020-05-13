75ffbbcef5f0cba107335d8ebd4524108c0caaa2
Zongwei Zhou <zongweiz@asu.edu>
2020-05-13 11:12:27 -0700

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -48,57 +48,15 @@ def bce_dice_loss(y_true, y_pred):
     return 0.5 * keras.losses.binary_crossentropy(y_true, y_pred) - dice_coef(y_true, y_pred)
 
 # Evaluation metric: IoU
-def compute_iou(img1, img2):
-
-    img1 = np.array(img1)
-    img2 = np.array(img2)
-
-    if img1.shape[0] != img2.shape[0]:
-        raise ValueError("Shape mismatch: the number of images mismatch.")
-    IoU = np.zeros( (img1.shape[0],), dtype=np.float32)
-    for i in range(img1.shape[0]):
-        im1 = np.squeeze(img1[i]>0.5)
-        im2 = np.squeeze(img2[i]>0.5)
-
-        if im1.shape != im2.shape:
-            raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
-
-        # Compute Dice coefficient
-        intersection = np.logical_and(im1, im2)
-
-        if im1.sum() + im2.sum() == 0:
-            IoU[i] = 100
-        else:
-            IoU[i] = 2. * intersection.sum() * 100.0 / (im1.sum() + im2.sum())
-        #database.display_image_mask_pairs(im1, im2)
-
-    return IoU
+def compute_iou(im1, im2):
+    overlap = (im1>0.5) * (im2>0.5)
+    union = (im1>0.5) + (im2>0.5)
+    return overlap.sum()/float(union.sum())
 
 # Evaluation metric: Dice
 def compute_dice(im1, im2, empty_score=1.0):
-    """
-    Computes the Dice coefficient, a measure of set similarity.
-    Parameters
-    ----------
-    im1 : array-like, bool
-        Any array of arbitrary size. If not boolean, will be converted.
-    im2 : array-like, bool
-        Any other array of identical size. If not boolean, will be converted.
-    Returns
-    -------
-    dice : float
-        Dice coefficient as a float on range [0,1].
-        Maximum similarity = 1
-        No similarity = 0
-        Both are empty (sum eq to zero) = empty_score
-
-    Notes
-    -----
-    The order of inputs for `dice` is irrelevant. The result will be
-    identical if `im1` and `im2` are switched.
-    """
-    im1 = np.asarray(im1).astype(np.bool)
-    im2 = np.asarray(im2).astype(np.bool)
+    im1 = np.asarray(im1>0.5).astype(np.bool)
+    im2 = np.asarray(im2>0.5).astype(np.bool)
 
     if im1.shape != im2.shape:
         raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
@@ -107,7 +65,6 @@ def compute_dice(im1, im2, empty_score=1.0):
     if im_sum == 0:
         return empty_score
 
-    # Compute Dice coefficient
     intersection = np.logical_and(im1, im2)
 
     return 2. * intersection.sum() / im_sum
